fb32085773829dd3e58f8c71bd1e82efbc16aef7
DSikes <david@sikespc.com>
2023-01-30 11:49:31 -0500

    Opened project and fixed VCS root for git

---
+++ AoC-2022-Cpp/Day 03a/cmake-build-debug/CMakeFiles/Day_01.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/source/repos/PockyBum522 Github/AdventOfCode/AoC-2022-Cpp/Day 03a/RawData/RawData.cpp" "CMakeFiles/Day_01.dir/RawData/RawData.cpp.obj" "gcc" "CMakeFiles/Day_01.dir/RawData/RawData.cpp.obj.d"
+  "D:/source/repos/PockyBum522 Github/AdventOfCode/AoC-2022-Cpp/Day 03a/main.cpp" "CMakeFiles/Day_01.dir/main.cpp.obj" "gcc" "CMakeFiles/Day_01.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AoC-2022-Cpp/Day 03a/cmake-build-debug/CMakeFiles/Day_01.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day_01.dir/RawData/RawData.cpp.obj"
+  "CMakeFiles/Day_01.dir/RawData/RawData.cpp.obj.d"
+  "CMakeFiles/Day_01.dir/main.cpp.obj"
+  "CMakeFiles/Day_01.dir/main.cpp.obj.d"
+  "Day_01.exe"
+  "Day_01.exe.manifest"
+  "Day_01.pdb"
+  "libDay_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
