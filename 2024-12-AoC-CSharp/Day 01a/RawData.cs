788f3a0e2436b8d3ffb6205caebde2d7b55a5880
DSikes <pockybum522@gmail.com>
2024-12-01 08:25:11 -0500

    About to click day 1

---
+++ 2024-12-AoC-CSharp/Day 01a/RawData.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpAoC2024;
+
+public class RawData
+{
+    public static string SampleData01 => """
+
+                                         """;
+
+    public static string ActualData01 => """
+                                         
+                                         """;
+}
