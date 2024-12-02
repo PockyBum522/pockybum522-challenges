cdf843dd47f37f0d7d9d3c8d0c41eb7e2620bbca
DSikes <pockybum522@gmail.com>
2024-12-02 06:10:12 -0500

    Set up day 03a project, trying some new things

---
+++ 2024-12-AoC-CSharp/Day 03a/ApplicationLogistics/LoggerSetup.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpAoC2024.ApplicationLogistics;
+
+public static class LoggerSetup
+{
+    /// <summary>
+    /// Full path to base folder for logs (the folder, not the log files themselves)
+    /// </summary>
+    private static string LogAppBasePath =>
+        Path.Combine(
+            ApplicationData.RootDirectory, 
+            "Logs");
+    
+    /// <summary>
+    /// Full path to a generic log filename, for Serilog
+    /// </summary>
+    private static string LogPath => 
+        Path.Combine(
+            LogAppBasePath,
+            $"{ApplicationData.Name}_.log");
+    
+    public static LoggerConfiguration ConfigureLogger()
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(LogPath) ?? "");
+
+        return new LoggerConfiguration()
+            .Enrich.WithProperty("Application", "AocCSharpContext")
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .WriteTo.File(LogPath, rollingInterval: RollingInterval.Hour);
+    }
+}
---
+++ 2024-12-AoC-CSharp/Day 03a/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using CSharpAoC2024.ApplicationLogistics;
+
+namespace CSharpAoC2024;
+
+internal static class Program
+{
+    private static readonly Serilog.Core.Logger _logger = LoggerSetup.ConfigureLogger()
+        .MinimumLevel.Warning()
+        .CreateLogger();
+    
+    private static readonly Stopwatch _elapsedTotal = new();
+    
+    private static void workLine(string line, int lineCounter)
+    {
+        
+    }
+    
+    private static void workAllLines(string[] allLinesSplit)
+    {
+        var answer = 0;
+        
+        var lineCounter = 0;
+        foreach (var line in allLinesSplit)
+        {
+            workLine(line, lineCounter++);
+        }
+        
+        _logger.Warning("Answer: {BiggestScore}", answer);
+    }
+    
+    internal static async Task Main(string[] args)
+    {
+        await Task.Delay(1); // Keep the linter happy
+        _elapsedTotal.Start();
+        _logger.Fatal("Starting!"); // Fatal 'cause we ALWAYS want to see this in log
+        
+        var textToWork = RawData.ActualData01;
+
+        var dataLines = textToWork.Split('\n');
+
+        workAllLines(dataLines);
+
+        logStopwatchFinalTimes();
+    }
+    
+    private static void logStopwatchFinalTimes()
+    {
+        _elapsedTotal.Stop();
+
+        var elapsedString = _elapsedTotal.Elapsed.ToString(@"hh\:mm\:ss\.fff");
+        _logger.Fatal("Final total time elapsed hh:mm:ss.fff: {TimeString}", elapsedString);    // Fatal 'cause we ALWAYS want to see this in log
+    }
+}
