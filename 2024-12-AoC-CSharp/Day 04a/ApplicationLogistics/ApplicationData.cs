162d854a8b758c3071c1b23a3669146e634d4cb5
David S <1970959+PockyBum522@users.noreply.github.com>
2024-12-04 09:29:15 -0500

    Made Day 04a initial project

---
+++ 2024-12-AoC-CSharp/Day 04a/ApplicationLogistics/ApplicationData.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpAoC2024.ApplicationLogistics;
+
+public static class ApplicationData
+{
+    /// <summary>
+    /// Full path to base folder for logs (the folder, not the log files themselves)
+    /// </summary>
+    public static string Name => "AocCSharp";
+    
+    /// <summary>
+    /// Full path to the directory the app is running from, used for building log and settings directories
+    /// </summary>
+    public static string RootDirectory =>
+        Path.GetDirectoryName(Environment.ProcessPath) ?? "ERROR GETTING APP PATH FOR AocCSharp";
+}
---
+++ 2024-12-AoC-CSharp/Day 04a/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using CSharpAoC2024.ApplicationLogistics;
+
+namespace CSharpAoC2024;
+
+internal static class Program
+{
+    private static string _textToWork =>  RawData.ActualData01;
+    
+    private static readonly Serilog.Core.Logger _logger = LoggerSetup.ConfigureLogger()
+        .MinimumLevel.Debug()
+        .CreateLogger();
+    
+    private static readonly Stopwatch _elapsedTotal = new();
+
+    private const string _validStartCharacters = "mul(";
+
+    private static void workLine(string line, int lineCounter)
+    {
+        
+    }
+    
+    private static void workAllLines(string[] allLinesSplit)
+    {
+        var answer = 0;
+        
+        var lineCounter = 0;
+        foreach (var line in allLinesSplit)
+        {
+            workLine(line, lineCounter++);
+        }
+        
+        _logger.Warning("Answer: {BiggestScore}", answer);
+    }
+    
+    internal static async Task Main(string[] args)
+    {
+        await Task.Delay(1); // Keep the linter happy
+        _elapsedTotal.Start();
+        _logger.Fatal("Starting!"); // Fatal 'cause we ALWAYS want to see this in log
+
+        var dataLines = _textToWork.Split('\n');
+
+        workAllLines(dataLines);
+
+        logStopwatchFinalTimes();
+    }
+    
+    private static void logStopwatchFinalTimes()
+    {
+        _elapsedTotal.Stop();
+
+        var elapsedString = _elapsedTotal.Elapsed.ToString(@"hh\:mm\:ss\.fff");
+        _logger.Fatal("Final total time elapsed hh:mm:ss.fff: {TimeString}", elapsedString);    // Fatal 'cause we ALWAYS want to see this in log
+    }
+}
