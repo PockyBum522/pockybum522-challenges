765b2d4c74949f9a2a6bb3b5801dbe285c3c1035
David S <1970959+PockyBum522@users.noreply.github.com>
2025-01-01 03:00:58 -0500

    Now I am become triangle

--- 2025-01-Genuary/Day01/GenuaryDayXX/GenuaryDayXX/Views/MainView.axaml.cs
+++ 2025-01-Genuary/Day01/GenuaryDayXX/GenuaryDayXX/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 
 namespace GenuaryDayXX.Views;
 
@@ -6,6 +9,23 @@ public partial class MainView : UserControl
 {
     public MainView()
     {
+        
         InitializeComponent();
+        
+        var maxHeight = 2088;
+        var maxWidth = 3840;
+        
+        for (var x = 0; x < maxWidth; x++)
+        {
+            var thisLine = new Line()
+            {
+                StartPoint = new Point(x, x),
+                EndPoint = new Point(x, maxHeight),
+                Stroke = Brushes.White,
+                StrokeThickness = 0.5
+            };
+            
+            Cv1.Children.Add(thisLine);
+        }
     }
 }
